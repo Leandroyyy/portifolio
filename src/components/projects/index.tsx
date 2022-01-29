9fdb6f890b63dcd3a11e77f464fe775f559928b8
Leandroyyy <leandro.cavallari123@gmail.com>
2022-01-29 18:20:20 -0300

    das

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,6 +1,13 @@
 import { Images, ProjectsBox, ProjectsContainer, Title } from "./Projects";
 import devFinance from '../../assets/img/devFinance.png'
+import {Data} from './projectsData'
 
+
+type ImagesProps = {
+    id:number,
+    title:string,
+    subTitle:string,
+}
 
 export function Projects(){
     return(
@@ -14,6 +21,17 @@ export function Projects(){
             {/*realizar uma troca de pagina mostrando imagem dos meus projetos*/}
 
             <Images src={devFinance} />
+
+            <div>
+                {/*Data.map((data: ImagesProps, index:number) => {
+                    return(
+                        <div>
+                            <Images />
+                          
+                        </div>
+                    )
+                })*/}
+            </div>
 
         </ProjectsBox>
     
