60c18468dd8494c659ce108bf6951b3faf9f8186
Leandroyyy <leandro.cavallari123@gmail.com>
2022-01-30 12:26:23 -0300

    asdsa

---
+++ src/components/graduation/Graduation.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const GraduationContainer = styled.section`
+
+    width:100vw;
+    height:100vh;
+    background:#000;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+
+    h1{
+        color:white;
+    }
+
+`
---
+++ src/components/graduation/index.tsx
@@ -0,0 +1,10 @@
+import { GraduationContainer } from "./Graduation";
+
+
+export function Graduation(){
+    return(
+        <GraduationContainer>
+            <h1>Graduation</h1>
+        </GraduationContainer>
+    )
+}
