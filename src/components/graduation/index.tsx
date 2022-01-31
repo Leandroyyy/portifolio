ab0eb07ca3019c5f6b9f3b30f14debc5dcd1bc96
Leandroyyy <leandro.cavallari123@gmail.com>
2022-01-31 08:13:30 -0300

    dasdsa

--- src/components/graduation/index.tsx
+++ src/components/graduation/index.tsx
@@ -17,7 +17,7 @@ import {
   Typescript,
   Vue,
 } from "./Graduation";
-import Fiap from "../../assets/img/fiap.png";
+import Fiap from "../../assets/img/Fiap.png";
 import Python from '../../assets/img/python.svg'
 import Alura from '../../assets/img/alura.png'
 import Java from '../../assets/img/java.svg'
